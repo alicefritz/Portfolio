a4f1f0b800afa8a28fb1877f47151b2b5c8eedd7
alicefritz <44866891+alicefritz@users.noreply.github.com>
2021-02-17 15:46:55 +0100

    Add files via upload

---
+++ precache-manifest.45ee7613c3fc2aa1f68c3ce829ad5fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10684467b49e65e49fca45b2d60cf41",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e35887bcad477e59a42",
+    "url": "./static/css/main.78e08bef.chunk.css"
+  },
+  {
+    "revision": "b193c6d69c6f9dead198",
+    "url": "./static/js/2.bc270869.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.bc270869.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e35887bcad477e59a42",
+    "url": "./static/js/main.4e4627f0.chunk.js"
+  },
+  {
+    "revision": "153b1044f31d40554c71",
+    "url": "./static/js/runtime-main.6bfafd9d.js"
+  }
+]);
